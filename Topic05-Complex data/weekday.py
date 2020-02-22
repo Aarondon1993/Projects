8fc1c473a87e454f7c781a8fd00c87ef942ad686
Aarondon1993 <aarondonnelly@live.ie>
2020-02-22 17:57:31 +0000

    updated weekday.py

--- Topic05-Complex data/weekday.py
+++ Topic05-Complex data/weekday.py
@@ -6,7 +6,7 @@ import datetime
 # This will import the datetime module
 now = datetime.datetime.now()
 # This allows me to print the current date
-if now.weekday()== 0 or 1 or 2 or 3 or 4:
+if now.weekday() in range(0,4):
 #This allows me to determine which day it is based on the datetime
 # 0 = Monday, 1= Tuesday... and so on
  print('Unfortuntely today is a weekday')
