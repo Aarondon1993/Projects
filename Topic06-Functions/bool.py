1979c86a44814f72b3bb4d6cfe868ef82d4e0d2c
Aarondon1993 <aarondonnelly@live.ie>
2020-03-07 20:15:31 +0000

    Adding Topic 6 task

---
+++ Topic06-Functions/bool.py
@@ -0,0 +1,2 @@
+bool
+profitable=True
---
+++ Topic06-Functions/squareroot.py
@@ -0,0 +1,9 @@
+# Aaron Donnelly
+# Calculating the approximate square root of a float number.
+Percision= 0.001
+Y= float(input("Please input the number you want the square root of:"))
+g= Y/2.0
+
+while abs(g*g-Y)>= Percision:
+    g= g-(((g**2)-Y)/(2*g))
+print (" Square root of ", Y, "is approximately", g)
