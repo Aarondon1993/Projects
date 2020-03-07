cabc8ca60ba273bff38f98340a2741f9cbbac73d
Aarondon1993 <aarondonnelly@live.ie>
2020-03-07 20:23:32 +0000

    Updated comments

--- Topic07-Reading files/readfile.py
+++ Topic07-Reading files/readfile.py
@@ -1,5 +1,6 @@
 # Aaron Donnelly
-# This program will count the number of re=occurences of the letter 'e' in a txt file
+# This program will count the number of re-occurences of the letter 'e' in a txt file titled moby-dick.py
 f= open("moby-dick.txt", "r")
 x= f.read().count('e')
 print(x)
+
