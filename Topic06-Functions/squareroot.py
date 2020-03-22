22a04961e1dc5e3eb5442e0ec76b258386c2409f
Aarondon1993 <aarondonnelly@live.ie>
2020-03-22 12:19:08 +0000

    Updated square root to add function

--- Topic06-Functions/squareroot.py
+++ Topic06-Functions/squareroot.py
@@ -1,5 +1,8 @@
 # Aaron Donnelly
 # Calculating the approximate square root of a float number.
+
+def square_root(y):
+    return(g)
 Percision= 0.001
 Y= float(input("Please input the number you want the square root of:"))
 g= Y/2.0
