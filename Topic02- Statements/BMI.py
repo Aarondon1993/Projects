04aebe28912ba341fb6432ce68c6d2eb209b3495
Aarondon1993 <aarondonnelly@live.ie>
2020-04-12 17:32:37 +0100

    Updated BMI program

--- Topic02- Statements/BMI.py
+++ Topic02- Statements/BMI.py
@@ -1,6 +1,18 @@
+a= 18.5
+b=25.0
+
 print("This program will calculate you body mass index (BMI)")
 weight= float(input("Insert you weight in kg:"))
 height= float(input("Insert you height in cm:"))
 
 BMI= weight / (height/100 * height/100)
 print("BMI={0:.2f}".format(BMI))
+if BMI < a:
+        print ("You are underweight")
+        
+if BMI > b:
+        print ("You are overweight")
+        
+if BMI  >= a and BMI <= b:
+        print ("You are a healthy weight")
+                   
